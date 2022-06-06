7253a1f260708f6bf0a8bfda60c694e07d1260ff
Szilasy Imola <89708149+SZIMOLA@users.noreply.github.com>
2022-06-06 13:10:23 +0200

    Add files via upload

--- js/custom-js.js
+++ js/custom-js.js
@@ -1,9 +1,16 @@
- $(document).ready(function() {
-            $('.submit').click(function() {
-                var number = $('input[name = "rating"]:checked').val();
-                $('.number').html(number);
-                $('.content').first().hide();
-                $('.content').last().show();
-                $('.thank-box').fadeIn(400);
-            });
-        });
+ $(document).ready(function () {
+
+     $('.rating label').click(function () {
+
+         var key = $(this).attr("for");
+         var number = parseInt($('input[id=' + key + ']').val());
+           $('.number').html(number);
+         if (number !== 0) {
+             $('.submit').click(function () {
+                 $('.content').first().hide();
+                 $('.content').last().show();
+                 $('.thank-box').fadeIn(400);
+             });
+         };
+     });
+ });
